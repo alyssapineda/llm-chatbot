64833faa29cfdd43bd3806105d9ddb5eee27a77f
alyssa <110448688+alyssapineda@users.noreply.github.com>
2023-09-28 13:24:08 +1300

    bug fixes

--- app.py
+++ app.py
@@ -2,72 +2,63 @@ import openai
 import re
 import streamlit as st
 from prompt import get_system_prompt
+# from snowflake.snowpark import Session
 
-st.title("☃️ Frosty the Chatbot")
+st.title("☃️ Frosty")
 
-#Initialising chat messages history
+# Initialize the chat messages history
 openai.api_key = st.secrets.OPENAI_API_KEY
+if "messages" not in st.session_state:
+    # system prompt includes table information, rules, and prompts the LLM to produce
+    # a welcome message to the user.
+    st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
 
-if "messages" not in st.session_state.keys():
-  #system prompt includes table info, rules and prompts the LLM to produce
-  #welcome message to user
-  st.session_state.messages = [
-    {"role": "system", "content": get_system_prompt}
-  ]
-
-#Prompting for user input and saving this in session state, creates textbox
+# Prompt for user input and save
 if prompt := st.chat_input():
-  st.session_state.messages.append({"role": "user", "content":prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-#Display the existing chat messages
+# display the existing chat messages
 for message in st.session_state.messages:
-  #if message is from system, skips the current iteration of the loop, system messages are not displayed
-  if message["role"] == "system":
-    continue
-  #set role for subsequent text or data display within UI
-  with st.chat_message(message["role"]):
-    #displays content of message
-    st.write(message["content"])
-    #if results are found in message, display a DataFrame/render structured data in tabular format within Streamlit
-    if "results" in message:
-      st.dataframe(message["results"])
-
+    #if message is from system, skips the current iteration of the loop, system messages are not displayed
+    if message["role"] == "system":
+        continue
+    #set role for subsequent text or data display within UI
+    with st.chat_message(message["role"]):
+        #displays content of message
+        st.write(message["content"])
+        #if results are found in message, display a DataFrame/render structured data in tabular format within Streamlit
+        if "results" in message:
+            st.dataframe(message["results"])
 
 #If last message is not from assistant, generate a new response
 #instead of displaying entire response, use OpenAI stream to signify
 if st.session_state.messages[-1]["role"] != "assistant":
-  # Call LLM
-  with st.chat_message("assistant"):
+    with st.chat_message("assistant"):
+        response = ""
+        # Create an empty Streamlit container for displaying the response
+        resp_container = st.empty()
+        #Iterate through response chunks from model in streaming mode
+        for delta in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+            stream=True,
+        ):
+            #Append each response chunk
+            response += delta.choices[0].delta.get("content", "")
+            #Display the cumulative response in resp_container using markdown formatting
+            resp_container.markdown(response)
 
-    #Create chat message from assistant
-    response = ""
-    resp_container = st.empty() # Create an empty Streamlit container for displaying the response
-    
-    #Iterate through response chunks from model in streaming mode
-    for delta in openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-      stream=True,
-
-    ):
-      #Append each response chunk
-      response += delta.choices[0].delta.get("content", "")
-
-      #Display the cumulative response in resp_container using markdown formatting
-      resp_container.markdown(response)
-
-    #Create message dictionary for assistant's response
-    message = {"role": "assistant", "content": response}
-
-    #Parse response for a SQL query and execute if available
-    sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
-    if sql_match:
-      # Extract the SQL query from the matched pattern
-      sql = sql_match.group(1)
-      conn = st.experimental_connection("snowpark")
-      #Execute SQL query and store results in message dictionary
-      message["results"] = conn.query(sql)
-      #display query results in Streamlit DataFrame
-      st.dataframe(message["results"])
-    #Append assistant message including sql results to session's message history
-    st.session_state.messages.append(message)
+        #Create message dictionary for assistant's response
+        message = {"role": "assistant", "content": response}
+        # Parse the response for a SQL query and execute if available
+        sql_match = re.search(r"```sql\n(.*)\n```", response, re.DOTALL)
+        if sql_match:
+            # Extract the SQL query from the matched pattern
+            sql = sql_match.group(1)
+            conn = st.experimental_connection("snowpark")
+            #Execute SQL query and store results in message dictionary
+            message["results"] = conn.query(sql)
+            #display query results in Streamlit DataFrame
+            st.dataframe(message["results"])
+        #Append assistant message including sql results to session's message history
+        st.session_state.messages.append(message)
--- prompt.py
+++ prompt.py
@@ -2,41 +2,36 @@ import streamlit as st
 
 #Represents fully qualified name of DB table, including DB name, schema name and table name
 QUALIFIED_TABLE_NAME = "COMPUTE_WH.CYBERSYN_FINANCIAL.FINANCIAL_ENTITY_ANNUAL_TIME_SERIES"
-
 TABLE_DESCRIPTION = """
 This table has various metrics for financial entities (also referred to as banks) since 1983.
 The user may describe the entities interchangeably as banks, financial institutions, or financial entities.
 """
-
 # This query is optional if running Frosty on your own table, especially a wide table.
 # Since this is a deep table, it's useful to tell Frosty what variables are available.
 # Similarly, if you have a table with semi-structured data (like JSON), it could be used to provide hints on available keys.
 # If altering, you may also need to modify the formatting logic in get_table_context() below.
-
-#SQL query string retrieving specific columns (VARIABLE_NAME, DEFINITION) from table FINANCIAL_ENTITY_ATTRIBUTES_LIMITED in Snowflake DB
 METADATA_QUERY = "SELECT VARIABLE_NAME, DEFINITION FROM COMPUTE_WH.CYBERSYN_FINANCIAL.FINANCIAL_ENTITY_ATTRIBUTES_LIMITED;"
 
-
 GEN_SQL = """
-You will be acting as an AI Snowflake SQL expert named Frosty.
-Your goal is to give correct, executable SQL queries to users.
+You will be acting as an AI Snowflake SQL Expert named Frosty.
+Your goal is to give correct, executable sql query to users.
 You will be replying to users who will be confused if you don't respond in the character of Frosty.
 You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
-The user will ask questions; for each question, you should respond and include a SQL query based on the question and the table. 
+The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
 
 {context}
 
 Here are 6 critical rules for the interaction you must abide:
 <rules>
-1. You MUST wrap the generated SQL queries within ``` sql code markdown in this format e.g
+1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
 ```sql
 (select 1) union (select 2)
 ```
 2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
 3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
-4. Make sure to generate a single Snowflake SQL code snippet, not multiple. 
-5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names.
-6. DO NOT put numerical at the very front of SQL variable.
+4. Make sure to generate a single snowflake sql code, not multiple. 
+5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names
+6. DO NOT put numerical at the very front of sql variable.
 </rules>
 
 Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
@@ -51,32 +46,31 @@ Now to get started, please briefly introduce yourself, describe the table at a h
 Then provide 3 example questions using bullet points.
 """
 
+
 #cache results of function get_table_context so no need to re-execut everytime function is called
 
 #get_table_context: takes table_name and table_description, splits it into db name, schema namem table_name
 @st.cache_data(show_spinner=False)
-def get_table_context(table_name: str, table_description: str):
-  table = table_name.split(".")
-  #establish connection to snowflake db using streamlit's experimental_connection function
-  conn = st.experimental_connection("snowpark")
-  #execute sql query using established DB connection
-  #fetch info about columns to specific table from SF DB
-  #Query uses db, schema and table obtained from table_name
-  columns = conn.query(f"""
-      SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
-      WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
-      """,
-  )
-  #create a formatted string representing column list and data types from database query result
-  columns = "\n".join(
-    #list comprehension iterates through columns in DB query and construct list of strings and formats it
+def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
+    table = table_name.split(".")
+    #establish connection to snowflake db using streamlit's experimental_connection function
+    conn = st.experimental_connection("snowpark")
+    #execute sql query using established DB connection
+    #fetch info about columns to specific table from SF DB
+    #Query uses db, schema and table obtained from table_name
+    columns = conn.query(f"""
+        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
+        """,
+    )
+    columns = "\n".join(
+        #list comprehension iterates through columns in DB query and construct list of strings and formats it
         [
             f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
             for i in range(len(columns["COLUMN_NAME"]))
         ]
-  )
-
-  context = f"""
+    )
+    context = f"""
 Here is the table name <tableName> {'.'.join(table)} </tableName>
 
 <tableDescription>{table_description}</tableDescription>
@@ -84,34 +78,31 @@ Here is the table name <tableName> {'.'.join(table)} </tableName>
 Here are the columns of the {'.'.join(table)}
 
 <columns>\n\n{columns}\n\n</columns>
-  """
-  
-
-  if metadata_query:
-    #execute metadata query to fetch metadata from DB table
-    metadata = conn.query(metadata_query)
-    #format metadata into readable string using a list comprehension
-    metadata = "\n".join(
-      [
-          #for each row of metadata, extract variable_name, and definition
-          f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
-          for i in range(len(metadata["VARIABLE_NAME"]))
-      ]
-    )
-    #append formatted metadata to context
-    context = context + f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
-  return context
+    """
+    if metadata_query:
+        #execute metadata query to fetch metadata from DB table
+        #format metadata into readable string using a list comprehension
+        metadata = conn.query(metadata_query)
+        metadata = "\n".join(
+            [
+                #for each row of metadata, extract variable_name, and definition
+                f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
+                for i in range(len(metadata["VARIABLE_NAME"]))
+            ]
+        )
+        #append formatted metadata to context
+        context = context + f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
+    return context
 
 def get_system_prompt():
-  table_context = get_table_context(
-    table_name=QUALIFIED_TABLE_NAME,
-    table_description=TABLE_DESCRIPTION,
-    metadata_query=METADATA_QUERY
-  )
-  return GEN_SQL.format(context=table_context)
+    table_context = get_table_context(
+        table_name=QUALIFIED_TABLE_NAME,
+        table_description=TABLE_DESCRIPTION,
+        metadata_query=METADATA_QUERY
+    )
+    return GEN_SQL.format(context=table_context)
 
-
-#DO streamlit run prompts.py to view initial system prompt in a Streamlit app
+# do `streamlit run prompts.py` to view the initial system prompt in a Streamlit app
 if __name__ == "__main__":
     st.header("System prompt for Frosty")
     st.markdown(get_system_prompt())
